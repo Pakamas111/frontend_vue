907b81c46f4995847c56a920b2d5274b19ed93a8
Pakamas111 <6310210267@psu.ac.th>
2023-09-20 13:06:07 +0700

    Frontend_lab11

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,107 +1,125 @@
 <template>
-    <div>
-        <v-data-table :headers="headers" :items="desserts" sort-by="calories" class="elevation-1">
-            <template v-slot:top>
-                <v-toolbar flat>
-                    <v-toolbar-title>จัดการข้อมูล</v-toolbar-title>
-                    <v-divider class="mx-4" inset vertical></v-divider>
-                    <v-spacer></v-spacer>
-                    <v-btn color="primary" dark class="mb-2" @click="openDialog('add', defaultItem)">
-                        เพิ่มข้อมูล
-                    </v-btn>
+  <div>
+    <v-data-table :headers="headers" :items="employeeItem" class="elevation-1">
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>จัดการข้อมูล</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            dark
+            class="mb-2"
+            @click="openDialog('add', defaultItem)"
+          >
+            เพิ่มข้อมูล
+          </v-btn>
+        </v-toolbar>
+      </template>
+      <template v-slot:[`item.roles`]="{ item }">
+        {{ item.role.name }}
+      </template>
 
-                </v-toolbar>
-            </template>
-            <template v-slot:[`item.actions`]="{ item }">
-                <v-icon small class="mr-2" @click="openDialog('edit', item)">
-                    mdi-pencil
-                </v-icon>
-                <v-icon small @click="deleteItem(item)">
-                    mdi-delete
-                </v-icon>
-            </template>
-            <template v-slot:no-data>
-                <v-btn color="primary" @click="initialize">
-                    Reset
-                </v-btn>
-            </template>
-        </v-data-table>
-        <v-dialog v-model="dialog" max-width="500px">
+      <template v-slot:[`item.actions`]="{ item }">
+        <v-icon small class="mr-2" @click="openDialog('edit', item)">
+          mdi-pencil
+        </v-icon>
+        <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+      </template>
 
+      <template v-slot:no-data>
+        <v-btn color="primary" @click="initialize"> Reset </v-btn>
+      </template>
+    </v-data-table>
+    <v-dialog v-model="dialog" max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">{{ formTitle }}</span>
+        </v-card-title>
 
-            <v-card>
-                <v-card-title>
-                    <span class="text-h5">{{ formTitle }}</span>
-                </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  v-model="editedItem.name"
+                  label="ชื่อ"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  v-model="editedItem.lastName"
+                  label="นามสกุล"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  v-model="editedItem.Salary"
+                  label="เงินเดือน"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  v-model="editedItem.role"
+                  label="ตำแหน่ง"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
 
-                <v-card-text>
-                    <v-container>
-                        <v-row>
-                            <v-col cols="12" sm="6" md="4">
-                                <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
-                            </v-col>
-                            <v-col cols="12" sm="6" md="4">
-                                <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
-                            </v-col>
-                            <v-col cols="12" sm="6" md="4">
-                                <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
-                            </v-col>
-                            <v-col cols="12" sm="6" md="4">
-                                <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
-                            </v-col>
-                            <v-col cols="12" sm="6" md="4">
-                                <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
-                            </v-col>
-                        </v-row>
-                    </v-container>
-                </v-card-text>
-
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" text @click="close">
-                        Cancel
-                    </v-btn>
-                    <v-btn color="blue darken-1" text @click="save(formTitle)">
-                        บันทึก
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="500px">
-            <v-card>
-                <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                    <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-                    <v-spacer></v-spacer>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
-    </div>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="close"> Cancel </v-btn>
+          <v-btn color="blue darken-1" text @click="save(formTitle)">
+            บันทึก
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog v-model="dialogDelete" max-width="500px">
+      <v-card>
+        <v-card-title class="text-h5"
+          >Are you sure you want to delete this item?</v-card-title
+        >
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="closeDelete()">Cancel</v-btn>
+          <v-btn color="blue darken-1" text @click="deleteItemConfirm()"
+            >OK</v-btn
+          >
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
   
 <script>
 export default {
 
     data: () => ({
-        editItemIndex: -1,
+        firstName:'',
+        lastName:'',
+        salary:'',
+        role:'',
         dialog: false,
         dialogDelete: false,
         headers: [
             {
-                text: 'Dessert (100g serving)',
+                text: 'ไอดี',
                 align: 'start',
                 sortable: false,
-                value: 'name',
+                value: 'id',
             },
-            { text: 'Calories', value: 'calories' },
-            { text: 'Fat (g)', value: 'fat' },
-            { text: 'Carbs (g)', value: 'carbs' },
-            { text: 'Protein (g)', value: 'protein' },
+            { text: 'ชื่อ', value: 'firstName' },
+            { text: 'นามสกุล ', value: 'lastName' },
+            { text: 'เงินเดือน ', value: 'salary' },
+            { text: 'ตำแหน่ง ', value: 'role' },
             { text: 'Actions', value: 'actions', sortable: false },
         ],
-        desserts: [],
+        employeeItem:[],
+
         editedIndex: -1,
         editedItem: {
             name: '',
@@ -117,7 +135,8 @@ export default {
             carbs: 0,
             protein: 0,
         },
-        formTitle: ''
+        formTitle: '',
+        employeeItem: []
     }),
 
     watch: {
@@ -129,11 +148,24 @@ export default {
         },
     },
 
-    created() {
+    created () {
+        console.log('data employ ====>')
         this.initialize()
     },
 
     methods: {
+        async initialize() {
+            this.employeeItem = []
+            try {
+                var data = await this.axios.get('http://localhost:9000/employee')
+                console.log('data employ ====>', data)
+                this.employeeItem = data.data
+                // this.employeeItem = 'ooo'
+            console.log('data employ ====>', this.employeeItem)
+            } catch (error){
+
+            }
+        },
         close() {
             this.dialog = false
             this.editItem = []
@@ -145,122 +177,71 @@ export default {
                 protein: 0,
             }
         },
-        save(action) {
-
+        async save(action) {
             if (action === 'เพิ่มข้อมูล') {
-                this.desserts.push(this.editedItem)
-            }else{
-                Object.assign(this.desserts[this.editItemIndex], this.editedItem)
+            //     this.desserts.push(this.editedItem)
+            // }else{
+            //     Object.assign(this.desserts[this.editItemIndex], this.editedItem)
+            // }
+            //     this.close()
+            var data = {
+                firstName:this.firstName,
+                lastName:this.lastName,
+                salary:this.salary,
+                role:this.roles
             }
-                this.close()
-            
-        },
-        openDialog(Action, item) {
+            console.log('data after send ====>',data)
+            try{
+                var dataResponse = await this.axiospost('http://localhost:9000/employee',data)
+                console.log('dataResponse ===>', dataResponse)
+            }catch(error){(error.message)}
+
+            }else{
+                Object.assign(this.desserts[this.editedIndex],this.editItem)
+            }
+            this.close()
+         },
+           openDialog(Action, item) {
             // console.log(Action, item);
             this.formTitle = ''
             if (Action === 'add') {
 
                 this.formTitle = "เพิ่มข้อมูล"
                 this.editedItem = item
-                this.dialog = true
 
             } else {
-                this.formTitle = "แก้ไขข้อมูล"
-                this.editItemIndex = this.desserts.indexOf(item)
-                this.editedItem = item
+
                 this.dialog = true
+                this.formTitle = "แก้ไขข้อมูล"
+                // this.editItemIndex = this.desserts.indexOf(item)
+                // this.editedItem = item
+                this.firstName = item.firstName
+                this.lastName = item.lastName
+                this.salary = item.salary
+                this.roles = item.roles.name
+                this.idEmployee = item.id
             }
         },
-        initialize() {
-            this.desserts = [
-                {
-                    name: 'Frozen Yogurt',
-                    calories: 159,
-                    fat: 6.0,
-                    carbs: 24,
-                    protein: 4.0,
-                },
-                {
-                    name: 'Ice cream sandwich',
-                    calories: 237,
-                    fat: 9.0,
-                    carbs: 37,
-                    protein: 4.3,
-                },
-                {
-                    name: 'Eclair',
-                    calories: 262,
-                    fat: 16.0,
-                    carbs: 23,
-                    protein: 6.0,
-                },
-                {
-                    name: 'Cupcake',
-                    calories: 305,
-                    fat: 3.7,
-                    carbs: 67,
-                    protein: 4.3,
-                },
-                {
-                    name: 'Gingerbread',
-                    calories: 356,
-                    fat: 16.0,
-                    carbs: 49,
-                    protein: 3.9,
-                },
-                {
-                    name: 'Jelly bean',
-                    calories: 375,
-                    fat: 0.0,
-                    carbs: 94,
-                    protein: 0.0,
-                },
-                {
-                    name: 'Lollipop',
-                    calories: 392,
-                    fat: 0.2,
-                    carbs: 98,
-                    protein: 0,
-                },
-                {
-                    name: 'Honeycomb',
-                    calories: 408,
-                    fat: 3.2,
-                    carbs: 87,
-                    protein: 6.5,
-                },
-                {
-                    name: 'Donut',
-                    calories: 452,
-                    fat: 25.0,
-                    carbs: 51,
-                    protein: 4.9,
-                },
-                {
-                    name: 'KitKat',
-                    calories: 518,
-                    fat: 26.0,
-                    carbs: 65,
-                    protein: 7,
-                },
-            ]
-        },
-
         editItem(item) {
-            this.editedIndex = this.desserts.indexOf(item)
-            this.editedItem = Object.assign({}, item)
-            this.dialog = true
+            console.log('ittem select', item)
+            console.log('index item',this.desserts.indexOf(item))
+            // this.editedIndex = this.desserts.indexOf(item)
+            // this.editedItem = Object.assign({}, item)
+            // this.dialog = true
         },
 
         deleteItem(item) {
-            this.editedIndex = this.desserts.indexOf(item)
-            this.editedItem = item
+            this.idForDelete = item.id
             this.dialogDelete = true
         },
 
-        deleteItemConfirm() {
-            this.desserts.splice(this.editedIndex, 1)
-            this.closeDelete()
+        async deleteItemConfirm() {
+            try {
+                var response = await this.axios.delete('http://localhost:9000/employee'+ this.idForDelete)
+                this.initialize()
+            }catch(error){
+            }
+            this.closeDelete
         },
 
         closeDelete() {
@@ -269,10 +250,8 @@ export default {
             this.editedIndex = -1
 
         },
-
-        
     },
-}
+};
 </script>
   
 <style></style>
